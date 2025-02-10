f85b4b0ed849a63aff717bd3b0bf298eb53d30c4
MohamedArshath10 <arshath.m2003@gmail.com>
2025-02-10 23:05:43 +0530

    Frontend COmponents are created

---
+++ Frontend/src/Navigation/Navigation.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Navigation = () => {
+  return (
+    <NavStyled>
+        
+    </NavStyled>
+  )
+}
+
+const NavStyled = styled.div`
+
+`
+
+export default Navigation
